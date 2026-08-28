073c1e4b2e27e0fd893e6af21299e7a9d97d15fb
boatngin <thanapat.boat16@gmail.com>
2026-08-28 23:13:11 +0700

    init

---
+++ src/main/java/com/pegasus/pegasustcgapi/PegasusTcgApiApplication.java
@@ -0,0 +1,13 @@
+package com.pegasus.pegasustcgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegasusTcgApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PegasusTcgApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pegasus/pegasustcgapi/PegasusTcgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pegasus.pegasustcgapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegasusTcgApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
